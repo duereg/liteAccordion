a145ff41f7d142ebc8348eea5d4b5815fc297bed
Matt Blair <duereg@hotmail.com>
2011-06-02 18:00:08 +0800

    Rewrote Plugin to be more object oriented, also added support for resize (the original goal of the rewrite)

--- js/liteaccordion.jquery.js
+++ js/liteaccordion.jquery.js
@@ -5,14 +5,16 @@
 *	url:	  	http://nicolahibbert.com/horizontal-accordion-jquery-plugin
 *	demo:	  	http://www.nicolahibbert.com/demo/liteAccordion
 *
-*	Version:  	1.1.3
+*	Version:  	1.1.4
 *	Copyright: 	(c) 2010-2011 Nicola Hibbert
+*	Modified by Matt Blair on 2011-06-02 to allow for resizing
 *
 /*************************************************/
 ;(function($) {
-	
-	$.fn.liteAccordion = function(options) {
-				
+
+	// core utility and animation methods
+	function utils(target, options) {
+			
 		// defaults
 		var defaults = {
 			containerWidth : 960,
@@ -31,148 +33,246 @@
 			theme : 'basic', // basic, light*, dark, stitch*
 			rounded : false,
 			enumerateSlides : false
-		},
-		
-		// merge defaults with options in new settings object				
-			settings = $.extend({}, defaults, options),
+		};
 	
-		// define key variables
-			$accordion = this,
-			$slides = $accordion.find('li'),
-			slideLen = $slides.length,
-			slideWidth = settings.containerWidth - (slideLen * settings.headerWidth),
-			$header = $slides.children('h2'),
+		var $accordion 	= $(target);
+		var data 		= $accordion.data("liteAccordion");
+		if(!data){
+			data = {};
+			data.settings	= $.extend({}, defaults, options);
+			data.$slides 	= $accordion.children('ol').children('li');
+			data.$header 	= data.$slides.children('h2');
+			data.slideLen	= data.$slides.length;
+			data.slideWidth	= data.settings.containerWidth - (data.slideLen * data.settings.headerWidth);
+			data.playing 	= 0;
+			data.sentinel	= false;
 			
-		// core utility and animation methods
-			utils = {
-				getGroup : function(pos, index) {		
-					if (this.offsetLeft === pos.left) {
-						return $header.slice(index + 1, slideLen).filter(function() { return this.offsetLeft === $header.index(this) * settings.headerWidth });
-					} else if (this.offsetLeft === pos.right) {
-						return $header.slice(0, index + 1).filter(function() { return this.offsetLeft === slideWidth + ($header.index(this) * settings.headerWidth) });	
-					} 					
-				},
-				nextSlide : function(slideIndex) {
-					var slide = slideIndex + 1 || settings.firstSlide;
-
-					// get index of next slide
-					return function() {
-						return slide++ % slideLen;
-					}
-				},
-				play : function(slideIndex) {
-					var getNext = utils.nextSlide((slideIndex) ? slideIndex : ''), // create closure
-						start = function() {
-							$header.eq(getNext()).click();
-						};
-					
-					utils.playing = setInterval(start, settings.cycleSpeed);			
-				},
-				pause : function() {
-					clearInterval(utils.playing);
-				},
-				playing : 0,
-				sentinel : false
-			};		
-		
-		// set container heights, widths, theme & corner style
-		$accordion
-			.height(settings.containerHeight)
-			.width(settings.containerWidth)
-			.addClass(settings.theme)
-			.addClass(settings.rounded && 'rounded');
-		
-		// set tab width, height and selected class
-		$header
-			.width(settings.containerHeight)
-			.height(settings.headerWidth)
-			.eq(settings.firstSlide - 1).addClass('selected');
-		
-		// ie :(
-		if ($.browser.msie) {
-			if ($.browser.version.substr(0,1) > 8) {
-				$header.css('filter', 'none');
-			} else if ($.browser.version.substr(0,1) < 7) {
-				return false;
-			}
+			$accordion.data("liteAccordion", data);
+		} else {
+			data.settings 	= $.extend({}, defaults, options);
+			data.slideWidth = data.settings.containerWidth - (data.slideLen * data.settings.headerWidth);
+			$accordion.data("liteAccordion", data);
 		}
 		
-		// set initial positions for each slide
-		$header.each(function(index) {
-			var $this = $(this),
-				left = index * settings.headerWidth;
-				
-			if (index >= settings.firstSlide) left += slideWidth;
+		this.setCss = function() {
+			data = $accordion.data("liteAccordion");
+		
+			// set container theme & corner style
+			$accordion 
+				.addClass(data.settings.theme)
+				.addClass(data.settings.rounded && 'rounded');
 			
-			$this
-				.css('left', left)
-				.next()
-					.width(slideWidth)
-					.css({ left : left, paddingLeft : settings.headerWidth });
-			
-			// add number to bottom of tab
-			settings.enumerateSlides && $this.append('<b>' + (index + 1) + '</b>');			
-
-		});	
-				
-		// bind event handler for activating slides
-		$header.click(function(e) {
-			var $this = $(this),
-				index = $header.index($this),
-				$next = $this.next(),
-				pos = {
-					left : index * settings.headerWidth,
-					right : index * settings.headerWidth + slideWidth,
-					newPos : 0
-				}, 
-				$group = utils.getGroup.call(this, pos, index);
-								
-			// set animation direction
-			if (this.offsetLeft === pos.left) {
-				pos.newPos = slideWidth;
-			} else if (this.offsetLeft === pos.right) {
-				pos.newPos = -slideWidth;
+			if(data.settings.enumerateSlides) {
+				data.$header.each(function(index) {
+					var $this = $(this);
+					// add number to bottom of tab
+					$this.append('<b>' + (index + 1) + '</b>');	
+				});
 			}
 			
-			// check if animation in progress
-			if (!$header.is(':animated')) {
-
-				// activate onclick callback with slide div as context		
-				if (e.originalEvent) {
-					if (utils.sentinel === this) return false;
-					settings.onActivate.call($next);
-					utils.sentinel = this;
-				} else {
-					settings.onActivate.call($next);
-					utils.sentinel = false;
+			// selected class
+			data.$header
+				.eq(data.settings.firstSlide - 1)
+				.addClass('selected');	
+		
+			// ie :(
+			if ($.browser.msie) {
+				if ($.browser.version.substr(0,1) > 8) {
+					data.$header.css('filter', 'none');
+				} else if ($.browser.version.substr(0,1) < 7) {
+					return false;
 				}
-
-				// remove, then add selected class
-				$header.removeClass('selected').filter($this).addClass('selected');
+			} 
+		};
+		
+		this.dimension = function(newDimensions) { 
+			//reload data from storage
+			data = $accordion.data("liteAccordion");
 			
-				// get group of tabs & animate			
-				$group
-					.animate({ left : '+=' + pos.newPos }, settings.slideSpeed, function() { settings.slideCallback.call($next) })
-					.next()
-					.animate({ left : '+=' + pos.newPos }, settings.slideSpeed);
-						
+			$accordion
+				.height(data.settings.containerHeight)
+				.width(data.settings.containerWidth);
+				
+			data.$header
+				.width(data.settings.containerHeight)
+				.height(data.settings.headerWidth)
+				.next()
+				.width(data.slideWidth)
+				.css("padding-left", data.settings.headerWidth);
+		};
+		
+		this.positionElements = function() {
+			//reload data from storage
+			data = $accordion.data("liteAccordion");
+			var selectedIndex = data.$header.index(data.$slides.find("h2.selected"));
+			
+			data.$header.each(function(index) {
+				var $this = $(this);
+				var left = index * data.settings.headerWidth;
+					
+				if (index > selectedIndex) left += data.slideWidth;
+				
+				$this.css('left', left)
+					 .next() 
+					 .css('left', left); 		
+			}); 
+		};
+		  
+		this.handleEvents = function() {
+			handleHover();
+			handleClick();
+		};
+		
+		this.remove = function () { 
+			data.$header.unbind("click.liteAccordion");
+			$accordion.unbind("hover.liteAccordion");
+			$accordion.removeData('liteAccordion');
+		};
+		
+		function handleHover () {
+			// pause on hover			
+			if (data.settings.pauseOnHover) {
+				$accordion.bind("hover.liteAccordion", function() {
+					pause();
+				}, function() {
+					//reload data from storage
+					data = $accordion.data("liteAccordion");
+					
+					play(data.$header.index(data.$header.filter('.selected')));
+				});
 			}
-		});
+					
+			// start autoplay, call methods with no args = start from firstSlide
+			data.settings.autoPlay && play();
+		};
+		
+		function handleClick () { 
+	
+			// bind event handler for activating slides
+			data.$header.bind("click.liteAccordion", function(e) {
+				//reload data from storage
+				data = $accordion.data("liteAccordion");
+				
+				var $clicked = $(this),
+					$currSelected 	= data.$slides.find("h2.selected"),
+					clickedIndex = data.$header.index($clicked),
+					selectedIndex = data.$header.index($currSelected);
+				
+				//a different item has been selected that the currently selected item
+				if (clickedIndex != selectedIndex) {	
+					var $next = $clicked.next();
+					var $group = null;
+					var newPos = 0; 
+										
+					// set animation direction
+					if (clickedIndex < selectedIndex) {
+						newPos = data.slideWidth;
+						$group = data.$header.slice(clickedIndex + 1, selectedIndex + 1);
+					} else  {
+						newPos = -data.slideWidth;
+						$group = data.$header.slice(selectedIndex + 1, clickedIndex + 1)
+					}
+					
+					// check if animation in progress
+					if (!data.$header.is(':animated')) {
+
+						// activate onclick callback with slide div as context		
+						if (e.originalEvent) {
+							if (data.sentinel === this) return false;
+							data.settings.onActivate.call($next);
+							data.sentinel = this;
+						} else {
+							data.settings.onActivate.call($next);
+							data.sentinel = false;
+						}
+
+						// remove, then add selected class
+						data.$header.removeClass('selected')
+								    .filter($clicked)
+								    .addClass('selected');
+					
+						// get group of tabs & animate			
+						$group
+							.animate({ left : '+=' + newPos }, data.settings.slideSpeed, function() { data.settings.slideCallback.call($next) })
+							.next()
+							.animate({ left : '+=' + newPos }, data.settings.slideSpeed);
+					}
+				} 
+			});
+		};
+		
+		function nextSlide (slideIndex) { 
+			var slide = slideIndex + 1 || data.settings.firstSlide;
+
+			// get index of next slide
+			return function() {
+				return slide++ % data.slideLen;
+			}
+		};
+		
+		function pause() {
+			//reload data from storage
+			data = $accordion.data("liteAccordion");
 			
-		// pause on hover			
-		if (settings.pauseOnHover) {
-			$accordion.hover(function() {
-				utils.pause();
-			}, function() {
-				utils.play($header.index($header.filter('.selected')));
+			clearInterval(data.playing);
+		};
+		
+		function play (slideIndex) {
+			//reload data from storage
+			data = $accordion.data("liteAccordion");
+			
+			var getNext = nextSlide((slideIndex) ? slideIndex : ''), // create closure
+				start = function() {
+					data.$header.eq(getNext()).trigger("click.liteAccordion");
+				};
+			
+			data.playing = setInterval(start, data.settings.cycleSpeed);			
+		};
+	};		
+	
+	// core utility and animation methods
+	var	methods = {
+		
+		init : function(options) {	 
+			return this.each(function() {  
+				// merge defaults with options in new data.settings object				
+				var horAccordion = new utils(this, options);
+				
+				//Set basic properties (classes, etc)
+				horAccordion.setCss();
+									
+				//Set height and width of containers and headers
+				horAccordion.dimension();
+				
+				//set initial header & slide positions
+				horAccordion.positionElements();
+				
+				//Setup click, hover event handlers
+				horAccordion.handleEvents();
+			}); 
+		},
+		dimension : function(options) {
+			return this.each(function() {  
+				// merge defaults with options in new data.settings object				
+				var horAccordion = new utils(this, options);
+				
+				//Set height and width of containers and headers
+				horAccordion.dimension();  
+				
+				//set header & slide positions
+				horAccordion.positionElements();
 			});
 		}
-				
-		// start autoplay, call utils with no args = start from firstSlide
-		settings.autoPlay && utils.play();
-		
-		return $accordion;
-		
+	};		
+	
+	$.fn.liteAccordion = function(method) {
+		if ( methods[method] ) {
+			return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Method ' +  method + ' does not exist on jQuery.liteAccordion' );
+		}    			
 	};
 	
 })(jQuery);
